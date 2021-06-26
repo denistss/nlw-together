02871850ef256e24f2b565a793b1d4042e4ad2e5
Denis Tiago Silva <56735135+denistss@users.noreply.github.com>
2021-06-26 17:16:04 -0300

    Criado função de enviar like para o firebase

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,13 +1,12 @@
-import { FormEvent, useState } from 'react';
 import { useParams } from 'react-router-dom'
 
 import LogoImg from '../assets/images/logo.svg';
+
 import { Button } from '../components/Button';
 import { Question } from '../components/Question/index';
 import { RoomCode } from '../components/RoomCode';
-import { useAuth } from '../hooks/useAuth';
+// import { useAuth } from '../hooks/useAuth';
 import { useRoom } from '../hooks/useRoom';
-import { database } from '../services/firebase';
 
 import '../styles/room.scss';
 
@@ -16,37 +15,10 @@ type RoomParms = {
 }
 
 export function AdminRoom() {
-    const { user } = useAuth();
+    // const { user } = useAuth();
     const params = useParams<RoomParms>();
-    const [newQuestion, setNewQuestion] = useState('');
     const roomId = params.id;
     const {title, questions} = useRoom(roomId);
-    
-
-    async function handleSendQuestion(event: FormEvent) {
-        event.preventDefault();
-
-        if (newQuestion.trim() === '') {
-            return;
-        }
-        if (!user) {
-            throw new Error('You must be logged id');
-        }
-
-        const question = {
-            content: newQuestion,
-            author: {
-                name: user.name,
-                avatar: user.avatar
-            },
-            isHighlighted: false,
-            isAnswered: false
-        };
-
-        await database.ref(`rooms/${roomId}/questions`).push(question);
-
-        setNewQuestion('');
-    }
 
     return (
         <div id="page-room">
